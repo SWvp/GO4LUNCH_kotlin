580590a9040ca4e23f9211123b50369497d26b46
stephane warin <serkalidev@gmail.com>
2022-05-13 14:51:35 +0200

    ClickOnChoseRestaurantButtonUseCase AddChatMessageToFirestoreUseCase are now in Kotlin

--- app/src/main/java/com/kardabel/go4lunch/domain/usecase/ClickOnChoseRestaurantButtonUseCase.kt
+++ app/src/main/java/com/kardabel/go4lunch/domain/usecase/ClickOnChoseRestaurantButtonUseCase.kt
@@ -1,67 +1,53 @@
-package com.kardabel.go4lunch.domain.usecase;
+package com.kardabel.go4lunch.domain.usecase
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import java.time.Clock
+import java.time.LocalDate
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+class ClickOnChoseRestaurantButtonUseCase(
+    private val firebaseFirestore: FirebaseFirestore,
+    private val firebaseAuth: FirebaseAuth,
+    private val clock: Clock,
+) {
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
-public class ClickOnChoseRestaurantButtonUseCase {
-
-    private final FirebaseFirestore firebaseFirestore;
-    private final FirebaseAuth firebaseAuth;
-    private final Clock clock;
-
-    public ClickOnChoseRestaurantButtonUseCase(FirebaseFirestore firebaseFirestore,
-                                               FirebaseAuth firebaseAuth,
-                                               Clock clock) {
-        this.firebaseFirestore = firebaseFirestore;
-        this.firebaseAuth = firebaseAuth;
-        this.clock = clock;
-
+    private fun getDayCollection(): CollectionReference {
+        return firebaseFirestore.collection(LocalDate.now(clock).toString())
     }
 
-    public CollectionReference getDayCollection() {
-        return firebaseFirestore.collection(LocalDate.now(clock).toString());
-    }
+    fun onRestaurantSelectedClick(
+        restaurantId: String,
+        restaurantName: String,
+        restaurantAddress: String,
+    ){
 
-    // CHECK IF USER HAVE ALREADY SET THE CHOSEN RESTAURANT,
-    // IF YES, JUST DELETE,
-    // IF NO, DELETE OLD ONE AND CREATE A NEW CHOSEN RESTAURANT
-    public void onRestaurantSelectedClick(
-            String restaurantId,
-            String restaurantName,
-            String restaurantAddress) {
+        if (firebaseAuth.currentUser != null){
 
-        if (firebaseAuth.getCurrentUser() != null) {
+            val userId = firebaseAuth.currentUser!!.uid
+            val userName = firebaseAuth.currentUser!!.displayName
 
-            String userId = firebaseAuth.getCurrentUser().getUid();
-            String userName = firebaseAuth.getCurrentUser().getDisplayName();
-
-            Map<String, Object> userGotRestaurant = new HashMap<>();
-            userGotRestaurant.put("restaurantId", restaurantId);
-            userGotRestaurant.put("restaurantName", restaurantName);
-            userGotRestaurant.put("userId", userId);
-            userGotRestaurant.put("userName", userName);
-            userGotRestaurant.put("restaurantAddress", restaurantAddress);
+            val userGotRestaurant: MutableMap<String, Any> = HashMap()
+            userGotRestaurant["restaurantId"] = restaurantId
+            userGotRestaurant["restaurantName"] = restaurantName
+            userGotRestaurant["userId"] = userId
+            userGotRestaurant["userName"] = userName!!
+            userGotRestaurant["restaurantAddress"] = restaurantAddress
 
             getDayCollection()
-                    .document(userId)
-                    .get()
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            if (Objects.equals(task.getResult().get("restaurantId"), restaurantId)) {
-                                task.getResult().getReference().delete();
-                            } else {
-                                task.getResult().getReference().set(userGotRestaurant);
-                            }
+                .document(userId)
+                .get()
+                .addOnCompleteListener { task: Task<DocumentSnapshot> ->
+                    if (task.isSuccessful) {
+                        if (task.result["restaurantId"] == restaurantId) {
+                            task.result.reference.delete()
+                        } else {
+                            task.result.reference.set(userGotRestaurant)
                         }
-                    });
+                    }
+                }
         }
     }
 }
