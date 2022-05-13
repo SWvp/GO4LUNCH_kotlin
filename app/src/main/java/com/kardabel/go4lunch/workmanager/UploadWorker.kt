fcf72f3a0068b609649fc38aa57f832975b4639c
stephane warin <serkalidev@gmail.com>
2022-05-14 01:31:19 +0200

    UploadWorker is now in Kotlin

--- app/src/main/java/com/kardabel/go4lunch/workmanager/UploadWorker.kt
+++ app/src/main/java/com/kardabel/go4lunch/workmanager/UploadWorker.kt
@@ -1,194 +1,149 @@
-package com.kardabel.go4lunch.workmanager;
+package com.kardabel.go4lunch.workmanager
 
-import android.annotation.SuppressLint;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.kardabel.go4lunch.R
+import com.kardabel.go4lunch.domain.model.UserWhoMadeRestaurantChoice
+import com.kardabel.go4lunch.presentation.ui.detailsview.RestaurantDetailsActivity
+import java.time.LocalDate
+import java.util.concurrent.ExecutionException
 
-import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.app.TaskStackBuilder;
-import androidx.work.WorkerParameters;
+class UploadWorker(
+    private val context: Context,
+    workerParams: WorkerParameters,
+) : Worker(context, workerParams) {
 
-import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.kardabel.go4lunch.R;
-import com.kardabel.go4lunch.domain.model.UserWhoMadeRestaurantChoice;
-import com.kardabel.go4lunch.presentation.ui.detailsview.RestaurantDetailsActivity;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-public class UploadWorker extends androidx.work.Worker {
-
-    private static final int NOTIFICATION_ID = 1;
-    private static final String CHANNEL_ID = "notif";
-
-    private String restaurantName = null;
-    private String restaurantAddress = null;
-    private String restaurantId = null;
-
-
-    private final Context context;
-
-    public UploadWorker(
-            @NonNull Context context,
-            @NonNull WorkerParameters workerParams) {
-
-        super(context, workerParams);
-
-        this.context = context;
-
+    companion object {
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "notif"
     }
 
-    @NonNull
-    @Override
-    public Result doWork() {
+    private var restaurantName: String? = null
+    private var restaurantAddress: String? = null
+    private var restaurantId: String? = null
 
-        List<UserWhoMadeRestaurantChoice> usersWithRestaurant = null;
+    override fun doWork(): Result {
+        var usersWithRestaurant: List<UserWhoMadeRestaurantChoice?>? = null
         try {
-            usersWithRestaurant = getUsersWithRestaurantChoice();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
+            usersWithRestaurant = getUsersWithRestaurantChoice()
+        } catch (e: ExecutionException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
         }
 
         if (usersWithRestaurant != null) {
-
             if (isCurrentUserHasChosenRestaurant(usersWithRestaurant)) {
 
-                List<String> workmates = new ArrayList<>();
+                val workmates = mutableListOf<String>()
 
-                String userId = context.getString(R.string.user_init_value);
-                if (FirebaseAuth
-                        .getInstance()
-                        .getCurrentUser() != null) {
-
-                    userId = FirebaseAuth
-                            .getInstance()
-                            .getCurrentUser()
-                            .getUid();
+                var userId = context.getString(R.string.user_init_value)
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    userId = FirebaseAuth.getInstance().currentUser!!.uid
                 }
-
-                for (UserWhoMadeRestaurantChoice userWhoMadeRestaurantChoice : usersWithRestaurant) {
-
-                    if (userWhoMadeRestaurantChoice
-                            .getRestaurantName()
-                            .equals(restaurantName) &&
-                            !userWhoMadeRestaurantChoice
-                                    .getUserId()
-                                    .equals(userId)) {
-                        workmates.add(userWhoMadeRestaurantChoice.getUserName());
-
+                for (userWithRestaurant in usersWithRestaurant) {
+                    if (userWithRestaurant!!.restaurantName.equals(restaurantName)
+                        && userWithRestaurant.userId.equals(userId)
+                    ) {
+                        workmates.add(userWithRestaurant.userName!!)
                     }
                 }
-                notificationMessage(workmates);
+                notificationMessage(workmates)
             }
         }
-
-        return Result.success();
-
+        return Result.success()
     }
 
-    private List<UserWhoMadeRestaurantChoice> getUsersWithRestaurantChoice() throws ExecutionException, InterruptedException {
-        List<UserWhoMadeRestaurantChoice> usersWithRestaurant = new ArrayList<>();
-
-
-        Tasks.await(getDayCollection().get().addOnCompleteListener(task -> {
-
-            for (QueryDocumentSnapshot document : task.getResult()) {
-                usersWithRestaurant.add(document.toObject(UserWhoMadeRestaurantChoice.class));
-            }
-
-        }));
-
-        return usersWithRestaurant;
+    @Throws(ExecutionException::class, InterruptedException::class)
+    private fun getUsersWithRestaurantChoice(): List<UserWhoMadeRestaurantChoice> {
+        val usersWithRestaurant: MutableList<UserWhoMadeRestaurantChoice> = ArrayList()
+        Tasks.await(getDayCollection().get()
+            .addOnCompleteListener { task: Task<QuerySnapshot> ->
+                for (document in task.result) {
+                    usersWithRestaurant.add(document.toObject(
+                        UserWhoMadeRestaurantChoice::class.java))
+                }
+            })
+        return usersWithRestaurant
     }
 
-    private boolean isCurrentUserHasChosenRestaurant(List<UserWhoMadeRestaurantChoice> usersWithRestaurant) {
-        boolean gotRestaurant = false;
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private fun getDayCollection(): CollectionReference {
+        return FirebaseFirestore.getInstance().collection(LocalDate.now().toString())
+    }
 
-        for (UserWhoMadeRestaurantChoice userWhoMadeRestaurantChoice : usersWithRestaurant) {
-
-            assert user != null;
-            if (userWhoMadeRestaurantChoice.getUserId().equals(user.getUid())) {
-                restaurantName = userWhoMadeRestaurantChoice.getRestaurantName();
-                restaurantAddress = userWhoMadeRestaurantChoice.getRestaurantAddress();
-                restaurantId = userWhoMadeRestaurantChoice.getRestaurantId();
-                gotRestaurant = true;
+    private fun isCurrentUserHasChosenRestaurant(usersWithRestaurant: List<UserWhoMadeRestaurantChoice?>): Boolean {
+        var gotRestaurant = false
+        val user = FirebaseAuth.getInstance().currentUser
+        for (restaurant in usersWithRestaurant) {
+            if (restaurant!!.userId == user!!.uid) {
+                restaurantName = restaurant.restaurantName
+                restaurantAddress = restaurant.restaurantAddress
+                restaurantId = restaurant.restaurantId
+                gotRestaurant = true
             }
         }
-        return gotRestaurant;
+        return gotRestaurant
     }
 
-    private void notificationMessage(List<String> allWorkmate) {
-        String notification;
-        StringBuilder stringBuilderWorkmates = new StringBuilder();
-        if (!allWorkmate.isEmpty()) {
-
-            for (int i = 0; i < allWorkmate.size(); i++) {
-                stringBuilderWorkmates.append(allWorkmate.get(i));
-                if (i < allWorkmate.size() - 2)
-                    stringBuilderWorkmates.append(context.getString(R.string.separate)).append(" ");
-                else if (i == allWorkmate.size() - 2)
-                    stringBuilderWorkmates.append(" ").append(context.getString(R.string.and)).append(" ");
+    private fun notificationMessage(allWorkmate: List<String>) {
+        val notification: String
+        val stringBuilderWorkmates = StringBuilder()
+        notification = if (allWorkmate.isNotEmpty()) {
+            for (i in allWorkmate.indices) {
+                stringBuilderWorkmates.append(allWorkmate[i])
+                if (i < allWorkmate.size - 2) stringBuilderWorkmates.append(context.getString(R.string.separate))
+                    .append(" ") else if (i == allWorkmate.size - 2) stringBuilderWorkmates.append(" ")
+                    .append(context.getString(R.string.and)).append(" ")
             }
-
-            notification = restaurantName
+            (restaurantName
                     + " "
                     + restaurantAddress
-                    + " " + stringBuilderWorkmates;
-
+                    + " " + stringBuilderWorkmates)
         } else {
-            notification = restaurantName
+            (restaurantName
                     + " "
-                    + restaurantAddress;
+                    + restaurantAddress)
         }
-
-        displayNotification(notification);
-
+        displayNotification(notification)
     }
 
-    public CollectionReference getDayCollection() {
-        return FirebaseFirestore.getInstance().collection(LocalDate.now().toString());
-    }
-
-
-    private void displayNotification(String notification) {
+    private fun displayNotification(notification: String) {
         // SET THE VIEW WHERE USER MUST GO WHEN TYPE ON NOTIFICATION
-        Intent intent = new Intent(context, RestaurantDetailsActivity.class);
-        intent.putExtra(RestaurantDetailsActivity.RESTAURANT_ID, restaurantId);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addNextIntentWithParentStack(intent);
-
-        @SuppressLint("UnspecifiedImmutableFlag")
+        val intent = Intent(context, RestaurantDetailsActivity::class.java)
+        intent.putExtra(RestaurantDetailsActivity.RESTAURANT_ID, restaurantId)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val stackBuilder = TaskStackBuilder.create(context)
+        stackBuilder.addNextIntentWithParentStack(intent)
+        @SuppressLint("UnspecifiedImmutableFlag") val pendingIntent
         //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        PendingIntent pendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
 
         // CREATE NOTIFICATION
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.lunch_icon)
-                .setContentTitle(context.getString(R.string.notification_title))
-                // .setContentText(notification)
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(notification))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.lunch_icon)
+            .setContentTitle(context.getString(R.string.notification_title))
+            //.setContentText(notification)
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(notification))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         // SHOW THE NOTIFICATION
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
-
-
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
     }
 }
